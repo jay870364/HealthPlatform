db818af7ecd8638495ff33b8109ed1922263a27c
DanteChang <jay870364@gmail.com>
2018-08-29 20:04:24 +0800

    0829 Minfo初版完成

--- Bossinfo.HealthPlatform/MInfo.aspx.cs
+++ Bossinfo.HealthPlatform/MInfo.aspx.cs
@@ -11,6 +11,14 @@ namespace Bossinfo.HealthPlatform
 {
     public partial class MInfo : System.Web.UI.Page
     {
+        public string htmlBMI = "";
+        public string htmlHeight = "";
+        public string htmlWeight = "";
+        public string htmlLowBP = "";
+        public string htmlHighBP = "";
+        public string htmlHeartBeat = "";
+        public string htmlBodyTemperture = "";
+
         protected void Page_Load(object sender, EventArgs e)
         {
             var uid = Request["UID"];
@@ -20,7 +28,7 @@ namespace Bossinfo.HealthPlatform
         }
         public void PrepareData(string uid)
         {
-           
+
 
             //將Request回來的UID解密
             var longUid = Convert.ToInt64(ToolLibs.DecryptDES(uid, Config.DESKey, Config.DESIV));
@@ -36,33 +44,44 @@ namespace Bossinfo.HealthPlatform
             //所記錄的測量資料
             var mIData = JsonConvert.DeserializeObject<Models.Utility.MIData>(measureInfo.MIData);
 
+            //舒張壓
             var tmpBP_Systolic = DBService.ResultRemark.GetRemarkByType(Enums.ResultRemarkType.LBP, ToolLibs.ConvertStrToDouble(mIData.BP_Systolic));
 
             System.Diagnostics.Debug.WriteLine($"{tmpBP_Systolic.Message}\t{tmpBP_Systolic.Level}");
 
+            //收縮壓
             var tmpHP_Diastolic = DBService.ResultRemark.GetRemarkByType(Enums.ResultRemarkType.HBP, ToolLibs.ConvertStrToDouble(mIData.BP_Diastolic));
 
             System.Diagnostics.Debug.WriteLine($"{tmpHP_Diastolic.Message}\t{tmpHP_Diastolic.Level}");
 
+            //脈博
             var tmpHeatRate = DBService.ResultRemark.GetRemarkByType(Enums.ResultRemarkType.HR, ToolLibs.ConvertStrToDouble(mIData.HeatRate));
 
             System.Diagnostics.Debug.WriteLine($"{tmpHeatRate.Message}\t{tmpHeatRate.Level}");
 
+            //BMI
             var tmpBMI = DBService.ResultRemark.GetRemarkByType(Enums.ResultRemarkType.BMI, ToolLibs.ConvertStrToDouble(mIData.BMI));
 
             System.Diagnostics.Debug.WriteLine($"{tmpBMI.Message}\t{tmpBMI.Level}");
 
-            Response.Write(measureInfo.MIData);
+            htmlBMI = mIData.BMI;
+
+            htmlHeight = mIData.Height;
+
+            htmlWeight = mIData.Weight;
+
+            htmlLowBP = mIData.BP_Systolic;
+
+            htmlHighBP = mIData.BP_Diastolic;
+
+            htmlHeartBeat = mIData.HeatRate;
+
+            htmlBodyTemperture = mIData.BodyTemperture;
+
+            
         }
-        /// <summary>
-        /// BMI
-        /// </summary>
-        /// <param name="Height"></param>
-        /// <param name="Weight"></param>
-        /// <returns></returns>
-        private double CalculateBMI(float Height, float Weight)
-        {
-            return Weight / Math.Pow((Height * 0.01), 2);
-        }
+
+        //Response.Write(measureInfo.MIData);
     }
+
 }
